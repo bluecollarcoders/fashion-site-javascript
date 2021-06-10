76d94428cfcf7c8e039ee0c87978d83a9501dbb0
Caleb Matteis <matteismusic@gmail.com>
2021-06-10 07:42:02 -0400

    commit

--- js/app.js
+++ js/app.js
@@ -5,16 +5,14 @@ window.onload = function() {
     let emailModal = document.getElementsByClassName('email-modal') [0];
 
     let closeModal = document.getElementsByClassName('email-modal__close-btn') [0];
-    console.log(closeModal);
-
-    console.log(emailModal);
+ 
     document.addEventListener('mouseleave', () => {
         emailModal.classList.add('email-modal__visible');
         console.log('mouse left')
 
     });
 
-    document.addEventListener('click', () => {
+    closeModal.addEventListener('click', () => {
         emailModal.classList.remove('email-modal__visible')
     } )
 
