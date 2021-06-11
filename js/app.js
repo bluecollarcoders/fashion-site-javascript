94148a2de76e7e9ed67589c0a53cd4141bcd8239
Caleb Matteis <matteismusic@gmail.com>
2021-06-11 07:37:09 -0400

    commit

--- js/app.js
+++ js/app.js
@@ -2,19 +2,52 @@
 
 window.onload = function() {
 
+    let emailState = false;
+
     let emailModal = document.getElementsByClassName('email-modal') [0];
 
     let closeModal = document.getElementsByClassName('email-modal__close-btn') [0];
- 
-    document.addEventListener('mouseleave', () => {
-        emailModal.classList.add('email-modal__visible');
-        console.log('mouse left')
 
+    let emailInput = document.getElementsByClassName('email-modal__input') [0];
+    
+    let emailButton = document.getElementsByClassName('email-modal__button') [0];
+    
+   function emailIsValid (email) {
+       return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)
+   }
+
+    emailButton.addEventListener('click', () => {
+        if (emailIsValid(emailInput.value)) {
+            console.log(emailInput.value)
+        } else {
+            document.getElementsByClassName('email-modal__form-group') [0].classList.add('email-modal__form-group--error');
+            document.getElementsByClassName('email-modal__error-message') [0].classList.add('email-modal__error-message--active');
+        }
+
+        console.log(emailInput.value)
     });
 
-    closeModal.addEventListener('click', () => {
+     closeModal.addEventListener('click', () => {
         emailModal.classList.remove('email-modal__visible')
     });
+
+    let showModal = () => {
+         if (emailState === false ) {
+            
+        emailModal.classList.add('email-modal__visible');
+        emailState = true;
+
+        }
+    };
+ 
+    document.body.addEventListener('mouseleave', () => {
+
+     showModal();
+    });
+
+   
+
+    
 
     
     console.log(document);
